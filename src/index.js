fa4c9ac01d7cad5870e8e7f30f0620eacd811385
Jordan <jordanjezequel@bbox.fr>
2018-10-24 17:01:25 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write('<a href="https://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html">To learn some mongoDB with NodeJS</a></br>');
+  res.write('<a href="https://www.npmjs.com/package/mustache">To learn some mustache template</a></br>');
+  res.write('<a href="https://expressjs.com/fr/guide/routing.html">Learn a bit of express for routing</a></br>');
+  res.end('<a href="http://docs.sequelizejs.com/manual/installation/getting-started">Using sequelize if needed</a>');
+  var MongoClient = require('mongodb').MongoClient;
+
+	var uri = "mongodb+srv://Shakai:Eu8tsS8k6cupaJ2P@cluster0-lbc8c.gcp.mongodb.net/test?retryWrites=true";
+	MongoClient.connect(uri, function(err, client) {
+	   const collection = client.db("test").collection("devices");
+	   // perform actions on the collection object
+	   var doc1 = {'hello':'doc1'};
+	   collection.insert(doc1);
+	   client.close();
+	});
+
+});
+server.listen(80);
+
+
+
+//Eu8tsS8k6cupaJ2P
