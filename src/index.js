c34003bf7242b34bbf161fc8c575617a1fc10ccf
GeoffroyBERNARD <geof.b@laposte.net>
2018-11-06 15:22:22 +0100

    Gestion bdd

--- src/index.js
+++ src/index.js
@@ -1,47 +1,37 @@
 let http = require('http');
 let express = require('express');
 let mongoose = require('mongoose');
-
-
-
-
 let app = express();
 let port = 80;
-
 let mustacheExpress = require('mustache-express');
-
 let uri = 'mongodb://mongo:27017/test';
-mongoose.connect(uri)
-
-// Register '.mustache' extension with The Mustache Express
+mongoose.connect(uri);
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
-  res.render('index', {
-    locals: {
-      title: 'Welcome'
-    }
-  });
+	res.render('index', {});
 });
 
+app.get('/signin', function (req, res) {
+	res.render('signin', {});
+});
+
+app.get('/login', function (req, res) {
+	res.render('login', {});
+});
+		 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-
+		
 	let UserSchema = new mongoose.Schema({
-		username: String,
 		password: String,
 		email: String,
 		boxes: Array
 	});
-	
-	let CategorySchema = new mongoose.Schema({
-		title: String,
-		prestations: Array
-	});
-
+		
 	let PrestationSchema = new mongoose.Schema({
 		title: String,
 		description: String,
@@ -49,7 +39,18 @@ db.once('open', function() {
 		price: Number,
 		isVisible: Boolean
 	})
-
+			
+	let CategorySchema = new mongoose.Schema({
+		title: String,
+		prestations: [PrestationSchema]
+	});
+		
+	let ContributionSchema = new mongoose.Schema({
+		name: String, 
+		Amount: Number,
+		message: String
+	})
+		
 	let BoxSchema = new mongoose.Schema({
 		recipientName: String,
 		recipientEmail: String,
@@ -60,26 +61,45 @@ db.once('open', function() {
 		isPaid: Boolean,
 		isOpened: Boolean,
 		isCurrent: Boolean,
-		prestations: Array,
-		contributions: Array
+		prestations: [PrestationSchema],
+		contributions: [ContributionSchema]
 	})
 
-	let ContributionSchema = new mongoose.Schema({
-		name: String, 
-		Amount: Number,
-		message: String
-	})
-	
 	let Box = mongoose.model('Box', BoxSchema);
 	let User = mongoose.model('User', UserSchema);
 	let Category = mongoose.model('Category', CategorySchema);
-	let Prestation = mongoose.model('prestation', PrestationSchema);
+	let Prestation = mongoose.model('Prestation', PrestationSchema);
 	let Contribution = mongoose.model('Contribution', ContributionSchema);
-	
-	
 
-	console.log("is okay");
-	
+/*
+	let user = new User({
+		password: "geof",
+		email: "geof.b@laposte.net",
+	});
+
+	user.boxes.push (new Box({
+		recipientName: "geof",
+		recipientEmail: "bern.geof@laposte.net",
+		message: "joyeux anniversaire",
+		isPaid: false,
+		isOpened: false,
+		isCurrent: false,
+	}));
+
+
+	user.save(function (err) {
+		if (err) return handleError(err);  
+	}); 
+
+	User.find(function (err, users) {
+		if (err) return console.error(err);
+		console.log("LESUSERS");
+		console.log(JSON.stringify(users));
+	})
+*/	 
+
+	console.log('Connection à la bdd effectuée');
+
 });
 
 
@@ -95,29 +115,3 @@ db.once('open', function() {
 
 app.listen(port);
 
-/*
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.write('<a href="https://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html">To learn some mongoDB with NodeJS</a></br>');
-  res.write('<a href="https://www.npmjs.com/package/mustache">To learn some mustache template</a></br>');
-  res.write('<a href="https://expressjs.com/fr/guide/routing.html">Learn a bit of express for routing</a></br>');
-  res.write('<a href="http://docs.sequelizejs.com/manual/installation/getting-started">Using sequelize if needed</a></br>');
-  res.end('<a href="https://mongoosejs.com/">An ORM for MongoDB</a></br>');
-  var MongoClient = require('mongodb').MongoClient;
-
-	var uri = "mongodb+srv://Shakai:Eu8tsS8k6cupaJ2P@cluster0-lbc8c.gcp.mongodb.net/test?retryWrites=true";
-	MongoClient.connect(uri, function(err, client) {
-	   const collection = client.db("test").collection("devices");
-	   // perform actions on the collection object
-	   var doc1 = {'hello':'doc1'};
-	   collection.insert(doc1);
-	   client.close();
-	});
-
-});
-server.listen(80);
-*/
-
-
-
-//Eu8tsS8k6cupaJ2P
