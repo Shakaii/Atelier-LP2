9887f46e56e4cff6b7b1c0ef902004db8a5e437c
Jordan <jordanjezequel@bbox.fr>
2018-11-07 13:08:31 +0100

    Can create a new box

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let http = require('http');
 let express = require('express');
 let mongoose = require('mongoose');
-let app = express(); 
+let app = express();
 let port = 80;
 let mustacheExpress = require('mustache-express');
 let uri = 'mongodb://mongo:27017/test';
@@ -12,43 +12,46 @@ app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
 
 let path = require('path');
-app.use(express.static(path.join(__dirname+'/public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 
-app.use(session({ secret: "secret"}));
+app.use(session({
+	secret: "secret"
+}));
 
-  
-/* GESTION DES GET */ 
+
+/* GESTION DES GET */
 app.get('/', function (req, res) {
 	let connected = false;
-	if (req.session.email){
+	if (req.session.email) {
 		connected = true;
 	}
-	res.render('index', {'connected': connected});
-}); 
+	res.render('index', {
+		'connected': connected
+	});
+});
 
 app.get('/signup', function (req, res) {
-	if (req.session.email){
+	if (req.session.email) {
 		res.redirect('/');
-	}else{
+	} else {
 		res.render('signup', {});
 	}
-	
+
 });
 
 app.get("/login", function (req, res) {
-	if (req.session.email){
+	if (req.session.email) {
 		res.redirect('/');
-	}
-	else{
+	} else {
 		res.render('login', {});
 	}
-	
+
 });
 
 app.get("/logout", function (req, res) {
@@ -58,41 +61,41 @@ app.get("/logout", function (req, res) {
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-		
+db.once('open', function () {
+
 	let userSchema = new mongoose.Schema({
 		password: String,
 		email: String,
 		boxes: Array,
 		isAdmin: Boolean
 	});
-		
+
 	let prestationSchema = new mongoose.Schema({
 		title: String,
 		description: String,
 		image: String,
 		price: Number,
 		isVisible: Boolean
-	})  
-			
+	})
+
 	let categorySchema = new mongoose.Schema({
 		title: String,
 		image: String,
 		prestations: [prestationSchema]
 	});
-		
+
 	let contributionSchema = new mongoose.Schema({
-		name: String,  
+		name: String,
 		Amount: Number,
 		message: String
 	})
-		
+
 	let boxSchema = new mongoose.Schema({
 		recipientName: String,
 		recipientEmail: String,
 		message: String,
 		urlGift: Number,
-		urlFund: Boolean,
+		urlFund: Number,
 		date: Date,
 		isPaid: Boolean,
 		isOpened: Boolean,
@@ -106,17 +109,17 @@ db.once('open', function() {
 	let Category = mongoose.model('Category', categorySchema);
 	let Prestation = mongoose.model('Prestation', prestationSchema);
 	let Contribution = mongoose.model('Contribution', contributionSchema);
- 
-	Category.find(function(err,test){
-		if (err) return handleError(err) 
+
+	Category.find(function (err, test) {
+		if (err) return handleError(err)
 		console.log(test[0].prestations)
-	}); 
+	});
 
 	//on signup
-	app.post('/signup', function (req, res) { 
+	app.post('/signup', function (req, res) {
 
 		//if the password matches the check 
-		if (req.body.passwordCheck == req.body.password){
+		if (req.body.passwordCheck == req.body.password) {
 
 			let user = new User({
 				password: req.body.password,
@@ -124,58 +127,106 @@ db.once('open', function() {
 			});
 
 			user.save(function (err) {
-				if (err) return handleError(err) 
+				if (err) return handleError(err)
 				req.session.email = user.email;
 				res.redirect('/');
-			}); 
-		} 
+			});
+		}
 	});
- 
+
 	//on login 
 	app.post("/login", function (req, res) {
-		
-		User.findOne({ email: req.body.mail }, function(err, user){
+
+		User.findOne({
+			email: req.body.mail
+		}, function (err, user) {
 			if (err) return handleError(err)
 
 			//if the passwords match
-			if (req.body.password == user.password ){
+			if (req.body.password == user.password) {
 				req.session.email = user.email;
 				res.redirect('/');
 			}
 		});
 	});
 
-	app.get("/catalog", function (req, res)  { 
+	app.get("/catalog", function (req, res) {
 
 		Category.find(function (err, categories) {
 			if (err) return console.error(err);
-			res.render('catalog', {'categories' : categories}); 	
+			res.render('catalog', {
+				'categories': categories
+			});
 		});
-	
+
+	});
+
+	app.get("/test", function (req, res) {
+
+		Box.findOne({isCurrent:'true'}, function (err, box) {
+			if (err) return console.error(err);
+			console.log(box);
+		});
+
+	});
+
+	app.get("/newBox", function (req, res) {
+
+		User.findOne({
+			email: req.session.email
+		}, function (err, user) {
+			if (err) return handleError(err)
+			let nBox = new Box({
+				recipientName: '',
+				recipientEmail: '',
+				message: '',
+				urlGift: '',
+				urlFund: '',
+				date: '',
+				isPaid: 'false',
+				isOpened: 'false',
+				isCurrent: 'true',
+				prestations: [],
+				contributions: []
+			});
+			nBox.save(function (err, nBox) {
+				if (err) return console.error(err);
+				console.log(nBox);
+				res.redirect('/');
+			});
+		});
 	});
 
 	app.get("/catalog/:category", function (req, res) {
 
 		Category
-		.findOne({ title: req.params.category})
-		.populate('prestations')
-		.exec(function (err, category){
-			if (err) return console.error(err);
-			Category.find(function (err, categories) {
+			.findOne({
+				title: req.params.category
+			})
+			.populate('prestations')
+			.exec(function (err, category) {
 				if (err) return console.error(err);
-				console.log(category);
-				res.render('prestations', {'categories' : categories, 'category' : category, 'prestations' : category.prestations});
+				Category.find(function (err, categories) {
+					if (err) return console.error(err);
+					console.log(category);
+					res.render('prestations', {
+						'categories': categories,
+						'category': category,
+						'prestations': category.prestations
+					});
+				});
 			});
-		}); 
 
-	}); 
-	   
+	});
+
 	app.get("/catalog/:category/:prestation", function (req, res) {
 
-		Category.findOne({ title: req.params.category},function (err, category) {
+		Category.findOne({
+			title: req.params.category
+		}, function (err, category) {
 			if (err) return console.error(err);
 
-			if (category){
+			if (category) {
 
 				let prestation = category.prestations.filter(function (prestation) {
 					return prestation.title === req.params.prestation;
@@ -183,64 +234,79 @@ db.once('open', function() {
 
 				Category.find(function (err, categories) {
 					if (err) return console.error(err);
-					res.render('prestation', {'categories' : categories, 'category' : category, 'prestation' : prestation});
+					res.render('prestation', {
+						'categories': categories,
+						'category': category,
+						'prestation': prestation
+					});
 				});
-					
-			}
-			else{
+
+			} else {
 				res.redirect('/catalog');
 			}
-		}); 
+		});
 	});
 
 
 	//profile
-	app.get("/profile",function(req,res){
+	app.get("/profile", function (req, res) {
 		//si connectedocker exec -i docker-node_mongo_1 mongo test --eval "db.dropDatabase()"
-		if (req.session.email){
+		if (req.session.email) {
 			//renvoie l'user
-			User.findOne({ email: req.session.email},function(err,user){
-				res.render('profile',{'connected': true, 'user':user});
+			User.findOne({
+				email: req.session.email
+			}, function (err, user) {
+				res.render('profile', {
+					'connected': true,
+					'user': user
+				});
 			});
 		}
 		//sinon accueil
-		else{ 
+		else {
 			res.redirect('/');
 		}
 	});
 
-	app.get("/profile/modify",function(req,res){
+	app.get("/profile/modify", function (req, res) {
 		//si connecte
-		if (req.session.email){
+		if (req.session.email) {
 			//renvoie l'user
-			User.findOne({ email: req.session.email},function(err,user){
-				res.render('modify',{'connected': true, 'user':user});
+			User.findOne({
+				email: req.session.email
+			}, function (err, user) {
+				res.render('modify', {
+					'connected': true,
+					'user': user
+				});
 			});
 		}
 		//sinon accueil
-		else{
+		else {
 			res.redirect('/');
 		}
 	});
 
 	//modifying password in profile
 	app.post("/profile/modify", function (req, res) {
-		
+
 		console.log(req.body.password);
 		//get logged in user
-		User.findOne({email: req.session.email }, function(err, user){
+		User.findOne({
+			email: req.session.email
+		}, function (err, user) {
 			if (err) return handleError(err)
 
 			//if the passwords match
-			if (req.body.passwordCheck == req.body.password){
+			if (req.body.passwordCheck == req.body.password) {
 				user.password = req.body.password;
 			}
-			 
+
 			user.save(function (err) {
-				if (err) return handleError(err) 
+				if (err) return handleError(err)
 				res.redirect('/profile');
 			});
-			
+
 		});
 
 	});
